e43789b974818b051054400bac4f160aae50db4e
lohitha <nlohitha97@gmail.com>
2026-08-04 16:40:52 +0530

    topic 23

---
+++ track/M02-Introduction-to-python/day5/src/calculation.py
@@ -0,0 +1,8 @@
+first_number = int(input("n:"))
+second_number = int(input("m:"))
+
+first_line = first_number/second_number
+second_line = first_number//second_number
+
+print(first_line)
+print(second_line)

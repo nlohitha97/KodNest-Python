95d46a3c0dba45b22d2a57ed4d04ae78b785f66d
lohitha <nlohitha97@gmail.com>
2026-08-04 16:05:54 +0530

    details

---
+++ track/M02-Introduction-to-python/day5/src/student_details.py
@@ -0,0 +1,14 @@
+# Read and convert the stident details
+name = input()
+age = int(input())
+course = float(input())
+
+# Display the values
+print(f"Student: {name}")
+print(f"Age: {age}")
+print(f"Course: {course}")
+
+# Display the data types
+print(type(name))
+print(type(age))
+print(type(course))

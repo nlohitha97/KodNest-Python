ac4694b45331f03fd4aaa573ac3574fba01b2df1
lohitha <nlohitha97@gmail.com>
2026-08-11 15:49:33 +0530

    Observe a Shared Default List

--- track/M02-Introduction-to-python/T04-Python_Functions/day-10/student_det.py
+++ track/M02-Introduction-to-python/T04-Python_Functions/day-10/student_det.py
@@ -3,4 +3,4 @@ def display_student(name,course,level):
 
 display_student("Aarav","Python","Beginner")
 display_student(name = "Meera",course = "Java",level = "Advanced")
-display_student("Sita",course ="python",level="Beginner")
+display_student("Sita",course = "python",level = "Beginner")

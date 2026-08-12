5a048719478861261700add37dc3f981a00511a5
lohitha <nlohitha97@gmail.com>
2026-08-12 16:42:29 +0530

    Student Profile using Classes and Objects

---
+++ track/M02-Introduction-to-python/T05-Classes and Objects/day-11/Student_profile.py
@@ -0,0 +1,23 @@
+class StudentProfile:
+    def __init__(self,student_id,name,course,score=0.0,skills = None,is_placed = False):
+        self.student_id = student_id
+        self.name = name
+        self.course = course
+        self.score = score
+        self.skills=[] if skills is None else list(skills)
+        self.is_placed = is_placed
+    
+    def __str__(self):
+        skills_text = (",".join(self.skills) if self.skills else "Not added")
+        placement_status = "Placed" if self.is_placed else "Not placed"
+        return(f"Student ID:{self.student_id}\n"
+                f"Student name:{self.name}\n"
+                f"Course: {self.course}\n"
+                f"Score:{self.course}\n"
+                f"Skilld:{self.skills}\n"
+                f"placement status:{self.is_placed}")
+    
+s = StudentProfile(101,"lohi","AIML",80.0,["PYTHON"])
+print(s)
+
+            

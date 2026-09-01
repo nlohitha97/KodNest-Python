cc1e7bd8e3f187ccc1d5bc0b1e992458efeceddb
lohitha <nlohitha97@gmail.com>
2026-09-01 15:14:16 +0530

    Extend a Parent Profile-Summary Method

---
+++ track/M03-Object-Oriented-Programming/T03-Inheritance,super() and Method overriding/Day20/extend_a_Parent.py
@@ -0,0 +1,19 @@
+class Profile:
+    def __init__(self,name):
+        self.name = name
+    def summary(self):
+        return f"Name: {self.name}"
+
+class StudentProfile(Profile):
+    def __init__(self,name,course):
+        super().__init__(name)
+        self.course = course
+        
+        #Override summary() here
+    def summary(self):
+        return f"{super().summary()} \nCourse: {self.course}"
+name=input().strip()
+course = input().strip()
+#Create object and display summary
+student = StudentProfile(name,course)
+print(student.summary())

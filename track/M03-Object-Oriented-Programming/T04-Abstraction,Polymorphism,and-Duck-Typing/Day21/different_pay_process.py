50fed448b27270c18d508c3fb831d8f3285193cc
lohitha <nlohitha97@gmail.com>
2026-09-03 17:44:07 +0530

    Build Different PAyment -Process Classess

--- track/M03-Object-Oriented-Programming/T04-Abstraction,Polymorphism,and-Duck-Typing/Day21/different_pay_process.py
+++ track/M03-Object-Oriented-Programming/T04-Abstraction,Polymorphism,and-Duck-Typing/Day21/different_pay_process.py
@@ -1,2 +1,45 @@
 # Build Different PAyment -Process Classess
 from abc import ABC,abstractmethod
+class PaymentProcessor(ABC):
+    @abstractmethod
+    def process_payment(self):
+        pass
+
+class UPIPayment(PaymentProcessor):
+    def __init__(self,amount):
+        self.amount = amount
+    #Implemrnt process_payment()
+    def process_payment(self):
+       return f"UPI Payment: {self.amount}"
+
+class CardPayment(PaymentProcessor):
+    def __init__(self,amount):
+        self.amount = amount
+    #Implemrnt process_payment()
+    def process_payment(self):
+       return f"Card Payment: {self.amount}"
+
+class NetBankingPayment(PaymentProcessor):
+    def __init__(self,amount):
+        self.amount = amount
+    #Implemrnt process_payment()
+    def process_payment(self):
+       return f"NetBanking Payment: {self.amount}"
+upi_amount = int(input())
+card_amount = int(input())
+net_amount = int(input())
+
+# Create the three objects
+
+UPI = UPIPayment(upi_amount)
+Card = CardPayment(card_amount)
+NetBanking = NetBankingPayment(net_amount)
+
+# Store them in one list
+
+pay = [UPI,Card,NetBanking]
+
+# Process them using one loop
+
+for i in pay:
+    print(i.process_payment())
